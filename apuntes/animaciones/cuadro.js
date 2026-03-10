90d4a1814bcac81b94eb9f5f70357521361ae1f8
valeria <vsantanamegia01.cf@iesesteveterradas.cat>
2026-03-10 17:57:15 +0100

    tables

--- apuntes/animaciones/cuadro.js
+++ apuntes/animaciones/cuadro.js
@@ -1,53 +1,163 @@
-$(document).ready(function() {
-    const speed = 250; // velocitat 250ms
+// Resol les següents preguntes:
+// 1. Carrega jQuery des de on volgueu.
 
-    // Funció DRY que fa tot: mou, canvia color, easing i log
-    function moveBox(direction) {
-        let props = {};
-        let easing = "";
-        let color = "";
-        let logMsg = "";
 
-        switch(direction) {
-            case "top":
-                props = { top: "-=100px", backgroundColor: "blue" };
-                easing = "linear";
-                logMsg = "Movent cap amunt";
+$(document).ready(function () {
+    // 2. Afegiu funcionalitat als quatre buttons que apareixen a la part superior de la finestra,
+    // cada cop que es faci click en una acció s'haurà de desplaçar 100 píxels en la direcció
+    // depenen del botó de direcció que s'hagui clickat.
+    // El moviment s'haurà de fer amb la funció nativa de jQuery ".animate()".
+
+    $("#top").on("click", function () {
+        $("#box1").animate({
+            top: "-=100px"
+        });
+    });
+
+    $("#bottom").on("click", function () {
+        $("#box1").animate({
+            top: "+=100px"
+        });
+    });
+
+    $("#left").on("click", function () {
+        $("#box1").animate({
+            left: "-=100px"
+        });
+    });
+
+    $("#right").on("click", function () {
+        $("#box1").animate({
+            left: "+=100px"
+        });
+    });
+
+    // 3. Afageix un key binding per a que les accions també responguin a les tecles de direcció o, en cas de no tenir aquestes, A, W, S i D.
+
+    $(document).keydown(function (event) {
+        switch (event.which) {
+            case 38:
+                $("#top").trigger("click");
                 break;
-            case "bottom":
-                props = { top: "+=100px", backgroundColor: "red" };
-                easing = "easeInOutBounce";
-                logMsg = "Movent cap avall";
+            case 40:
+                $("#bottom").trigger("click");
                 break;
-            case "left":
-                props = { left: "-=100px", backgroundColor: "green" };
-                easing = "easeInBack";
-                logMsg = "Movent cap esquerra";
+            case 37:
+                $("#left").trigger("click");
                 break;
-            case "right":
-                props = { left: "+=100px", backgroundColor: "yellow" };
-                easing = "swing";
-                logMsg = "Movent cap dreta";
+            case 39:
+                $("#right").trigger("click");
                 break;
-        }
-
-        console.log(logMsg);
-        $("#box1").animate(props, { duration: speed, easing: easing });
-    }
-
-    // Botons
-    $("#top").off("click").on("click", () => moveBox("top"));
-    $("#bottom").off("click").on("click", () => moveBox("bottom"));
-    $("#left").off("click").on("click", () => moveBox("left"));
-    $("#right").off("click").on("click", () => moveBox("right"));
-
-    // Teclat: fletxes i WASD
-    $(document).keydown(function(event) {
-        switch(event.which) {
-            case 38: case 87: moveBox("top"); break;    // ArrowUp / W
-            case 40: case 83: moveBox("bottom"); break; // ArrowDown / S
-            case 37: case 65: moveBox("left"); break;   // ArrowLeft / A
-            case 39: case 68: moveBox("right"); break;  // ArrowRight / D
         }
     });
+
+    // 4. Modifica les accions per a que la velocitat sigui "250".
+    $("#top").off("click").on("click", function () {
+        $("#box1").animate({ top: "-=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació amunt"); }
+        });
+    });
+
+    $("#bottom").off("click").on("click", function () {
+        $("#box1").animate({ top: "+=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació avall"); }
+        });
+    });
+
+    $("#left").off("click").on("click", function () {
+        $("#box1").animate({ left: "-=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació esquerra"); }
+        });
+    });
+
+    $("#right").off("click").on("click", function () {
+        $("#box1").animate({ left: "+=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació dreta"); }
+        });
+    });
+
+    // 5. Afegeix un log a la consola cada cop que s'executa una acció.
+    $("#top").off("click").on("click", function () {
+        $("#box1").animate({ top: "-=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació amunt"); }
+        });
+    });
+
+    $("#bottom").off("click").on("click", function () {
+        $("#box1").animate({ top: "+=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació avall"); }
+        });
+    });
+
+    $("#left").off("click").on("click", function () {
+        $("#box1").animate({ left: "-=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació esquerra"); }
+        });
+    });
+
+    $("#right").off("click").on("click", function () {
+        $("#box1").animate({ left: "+=100px" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació dreta"); }
+        });
+    });
+
+
+    // 6. Cada cop que s'executi una accio també caldra modificar més estils, en aquest cas
+    // el color de fons. A dalt (blau), a la dreta (groc), a l'esquerre (verd) i  avall (vermell).
+
+
+
+    $("#top").off("click").on("click", function () {
+        $("#box1").animate(
+            {
+                top: "-=100px",
+                backgroundColor: "blue"
+            },
+            {
+                duration: 250,
+                start: function () { console.log("Inici animació amunt"); }
+            });
+    });
+
+    $("#bottom").off("click").on("click", function () {
+        $("#box1").animate({ top: "+=100px", backgroundColor: "red" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació avall"); }
+        });
+    });
+
+    $("#left").off("click").on("click", function () {
+        $("#box1").animate({ left: "-=100px", backgroundColor: "yellow" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació esquerra"); }
+        });
+    });
+
+    $("#right").off("click").on("click", function () {
+        $("#box1").animate({ left: "+=100px", backgroundColor: "green" }, {
+            duration: 250,
+            start: function () { console.log("Inici animació dreta"); }
+        });
+    });
+
+    // 7. Canvia el estil de la corva de easing a la següent especificació:
+    // A dalt (linear), a la dreta (swing), a l'esquerre (easeInBack) i  avall (easeInOutBounce).
+
+
+
 });
+
+
+
+
+
+
+
